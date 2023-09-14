0df2bb99fd6dba8ec784e05ac40dd53fe846ef73
curiosityav <144117129+curiosityav@users.noreply.github.com>
2023-09-14 12:34:15 +0300

    Add files via upload

---
+++ lab/lab.cpp
@@ -0,0 +1,53 @@
+﻿
+
+
+#include <iostream>
+#include <stdLib.h>
+#include <Windows.h>
+
+using namespace std;
+int main()
+{
+    // Код програми до задачі 1 (HELLO)
+    cout << "HELLO!!!\n";
+    
+
+
+
+    cout << endl << endl << endl << endl;
+
+
+
+    //  Код програми до задачі 2 (SQUARE)
+
+    cout << "  -----------" << endl;
+    cout << " |           |" << endl;
+    cout << " |           |" << endl;
+    cout << " |           |" << endl;
+    cout << " |           |" << endl;
+    cout << " |           |" << endl;
+    cout << "  -----------" << endl;
+    
+    cout << endl << endl << endl << endl << endl;
+
+
+    // Код програми до задачі 3 (ANKETA)
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "\t       Прізвище: Волк " << endl;
+    cout << "\t           Ім'я: Аліна " << endl;
+    cout << "\t    По батькові: Вікторівна " << endl;
+    cout << "\t          Стать: жіноча " << endl;
+    cout << "\tДата народження: 2005-12-22 " << endl;
+    cout << "\t         Адреса: вул. Велика Діївська 40" << endl;
+    cout << "\t Номер телефону: +380 99 2927969 " << endl;
+    cout << "\t Місце навчання: ФПМ, ДНУ ім. Олеся Гончара " << endl;
+    cout << "\t          Група: ПА-23-1 " << endl;
+    cout << "\t           Хобі: Малювання та програмування" << endl;
+    system("pause");
+
+}
+
+
